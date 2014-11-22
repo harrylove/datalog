25a04545ac6c8fbdaf8ebd3d674efae39990d759
Harry Love <harrylove@gmail.com>
2014-11-22 12:20:33 -0800

    Duration seed data should be strings to match form input

--- server/things.js
+++ server/things.js
@@ -34,62 +34,62 @@ Meteor.startup(function() {
             {
                 user_id: user._id,
                 Date: '2014-05-06',
-                Duration: 1
+                Duration: '1'
             },
             {
                 user_id: user._id,
                 Date: '2014-06-06',
-                Duration: 2
+                Duration: '2'
             },
             {
                 user_id: user._id,
                 Date: '2014-06-07',
-                Duration: 4
+                Duration: '4'
             },
             {
                 user_id: user._id,
                 Date: '2014-05-06',
-                Duration: 5
+                Duration: '5'
             },
             {
                 user_id: user._id,
                 Date: '2014-10-06',
-                Duration: 10
+                Duration: '10'
             },
             {
                 user_id: user._id,
                 Date: '2014-07-11',
-                Duration: 10
+                Duration: '10'
             },
             {
                 user_id: user._id,
                 Date: '2014-08-12',
-                Duration: 7.4
+                Duration: '7.4'
             },
             {
                 user_id: user._id,
                 Date: '2014-11-09',
-                Duration: 3.4
+                Duration: '3.4'
             },
             {
                 user_id: user._id,
                 Date: '2014-12-06',
-                Duration: 1.2
+                Duration: '1.2'
             },
             {
                 user_id: user._id,
                 Date: '2014-02-23',
-                Duration: 5
+                Duration: '5'
             },
             {
                 user_id: user._id,
                 Date: '2014-09-23',
-                Duration: 20
+                Duration: '20'
             },
             {
                 user_id: user._id,
                 Date: '2014-05-16',
-                Duration: 3.4
+                Duration: '3.4'
             },
         ], function(thing) {
             Things.insert(thing);
